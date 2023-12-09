62881be922b9941c6c28f7880e304945b855aa8e
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-09 21:39:17 +0530

    forms

--- ANGULAR/myproject/src/app/addproduct/addproduct.component.ts
+++ ANGULAR/myproject/src/app/addproduct/addproduct.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'addproduct',
@@ -10,6 +12,26 @@ import { CommonModule } from '@angular/common';
 })
 export class AddproductComponent {
 
+  productForm!: FormGroup;
+  
+  isProductNameInvalid: boolean = false;
+  isProductPriceInvalid: boolean = false;
+
+  productService!:ProductService;
+
+  constructor(private fb: FormBuilder, productService:ProductService){
+    this.productForm = this.fb.group(
+      {
+        productName: ['', [Validators.required, Validators.minLength(3)]],
+        productPrice: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/), Validators.max(100000)]]
+      }
+    );   
+  }
+
+  addProduct():void{
+    console.log("Inside addProduct()");
+    //this.productService.addProduct(this.productForm.value.productName,this.productForm.value.productPrice, );    
+  }
 
 
 }
