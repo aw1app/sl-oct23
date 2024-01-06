55093b37b962fd56bb4f0ba7e8a316b38d7cc201
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-06 20:53:41 +0530

    update

---
+++ PHASE2/Healthcare/src/Doctor.java
@@ -0,0 +1,20 @@
+public class Doctor {
+
+	// attributes
+	int id;
+	String name;
+	float age;
+	char gender;
+	String designation;
+
+	// operations or methods
+
+	void examinePatient() {
+		System.out.println("Inside examinePatient method.");
+	}
+
+	void prescribeMedicine() {
+		System.out.println("Inside prescribeMedicine method.");
+	}
+
+}
