8fb32f544c7281ac8f2bc12d819b510bb048dc01
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-07 20:44:33 +0530

    update

---
+++ PHASE2/Healthcare/src/ArraysDemo.java
@@ -0,0 +1,15 @@
+
+public class ArraysDemo {
+
+	public static void main(String[] args) {
+		System.out.println("\n\n int array of size 10 DEMO \n");
+		
+		// Declare and initialize an integer array with 10 elements
+		int[] myArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+		
+		// Access and print the second element of the array
+		System.out.println(myArray[1]);
+
+	}
+
+}
