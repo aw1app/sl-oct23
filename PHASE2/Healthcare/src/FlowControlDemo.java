23d612bd5d35ec8855cbd608f3e19d890121c794
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-07 20:19:34 +0530

    update

--- PHASE2/Healthcare/src/FlowControlDemo.java
+++ PHASE2/Healthcare/src/FlowControlDemo.java
@@ -30,17 +30,44 @@ public class FlowControlDemo {
 		}
 
 		System.out.println("\n\n FOR-LOOP DEMO\n");
-		
+
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(i * i);
-		};
-		
+		}
+		;
+
 		System.out.println("\n\n FOR-LOOP DEMO-2\n");
-		for (int i = 1; i <= 10; i=i+2) {
+		for (int i = 1; i <= 10; i = i + 2) {
 			System.out.println(i * i);
 		}
+
+		// CHALLENGE-1: Req- Print cubes of even numbers b/w 5 to 10(inclusive of both
+		// numbers);
+		System.out.println("\n\n CHALLENGE-1 SOLUTION \n");
+		for (int i = 5; i <= 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i * i * i);
+			}
+		}
+
+		for (int i = 6; i <= 10; i = i + 2) {
+			System.out.println(i * i * i);
+		}
+		;
+
+		System.out.println("\n\n WHILE-LOOP DEMO-2\n");
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i * i);
+			i++;
+		};
 		
-		//CHALLENGE-1: Req- Print cubes of even numbers b/w 5 to 10;
+		System.out.println("\n\n DO-WHILE-LOOP DEMO-2\n");
+		int j = 15;
+		do {
+			System.out.println(j * j);
+			j++;
+		}while (j <= 10);
 
 	}
 
