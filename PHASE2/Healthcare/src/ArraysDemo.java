f7ef8c756dadc1897c9c5478ec684b23652558d6
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-07 20:49:31 +0530

    update

--- PHASE2/Healthcare/src/ArraysDemo.java
+++ PHASE2/Healthcare/src/ArraysDemo.java
@@ -3,15 +3,21 @@ public class ArraysDemo {
 
 	public static void main(String[] args) {
 		System.out.println("\n\n int array of size 10 DEMO \n");
-		
+
 		// Declare and initialize an integer array with 10 elements
 		int[] myArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-		
+
 		// Access and print the second element of the array
 		System.out.println(myArray[1]);
-		
+
 		// CHALLENGE-2 . Create a String array that holds names of 4 people.
 		// Loop thru and print all the names.
+		System.out.println("\n\n CHALLENGE-2 . Create a String array that holds names of 4 people DEMO \n");
+		String arr[] = { "name 1", "Name 2", "Name 3", "Name 4" };
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 
 	}
 
