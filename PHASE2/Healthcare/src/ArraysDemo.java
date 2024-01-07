de6d5060d3d4fb869c08368470c91a51b0bafe9e
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-07 21:30:59 +0530

    update

--- PHASE2/Healthcare/src/ArraysDemo.java
+++ PHASE2/Healthcare/src/ArraysDemo.java
@@ -18,6 +18,35 @@ public class ArraysDemo {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
+		;
+
+		System.out.println("\n\n Object array(of Doctor objects) of size 10 DEMO \n");
+		Doctor[] doctors = new Doctor[10];
+
+		doctors[0] = new Doctor();
+		doctors[1] = new Doctor();
+		doctors[2] = new Doctor();
+		// .....
+		// ..
+		doctors[9] = new Doctor();
+
+		// 2-d array
+		System.out.println("\n\n Defining and looping thru all elements(rows and cols) of a 2-d array DEMO \n");
+
+		int[][] numbers = { 
+				{ 1, 2, 3, 4, 5 },
+				{ 11, 12, 13, 14, 15 }
+				};
+
+		for (int i = 0; i < numbers.length; i++) {
+
+			System.out.println("Row "+i);
+			
+			for (int j = 0; j < numbers[i].length; j++) {				
+				System.out.println(numbers[i][j]);
+			}
+
+		}
 
 	}
 
