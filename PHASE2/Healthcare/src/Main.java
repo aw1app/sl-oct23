0b06be25b9c85918f1a1df977dcf64a4f403c0e2
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-13 22:54:46 +0530

    update

--- PHASE2/Healthcare/src/Main.java
+++ PHASE2/Healthcare/src/Main.java
@@ -7,30 +7,30 @@ public class Main {
 		Doctor d1 = new Doctor();
 
 		d1.name = "Sajesh";
-		d1.age = 24.7F;
+		//d1.age = 24.7F; access modifier error
 
 		Doctor d2 = new Doctor();
 
 		d2.name = "Tom";
-		d2.age = 29.1F;
+		//d2.age = 29.1F; //access modifier error
 		
 		
 		System.out.println("Doctor 1 name is "+d1.name);
 		System.out.println("Doctor 2 name is "+d2.name);
 		
 		Patient pat1 = new Patient();
-		pat1.name = "Sharma";
+		//pat1.name = "Sharma"; // access modifier error
 		
 		d1.examinePatient(pat1);
 		
 		//Conditionals Demo (Flow Control)
 		
-		if (d1.age < d2.age) {
+		if (d1.getAge() < d2.getAge() ) {
 		//....	
 			System.out.println("Doctor 1  is junior to Dctor 2");
 			//if() { }else {};
 			
-		}else if (d1.age > d2.age) {
+		}else if (d1.getAge() > d2.getAge() ) {
 			System.out.println("Doctor 1  is senior to Dctor 2");
 		}else {
 			System.out.println("Doctor 1  and Dctor 2 have same seniority");
