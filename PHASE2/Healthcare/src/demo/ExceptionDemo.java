1dac2a0bd2ed6aa2981e5ed50b4f74988ee8c18d
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-14 20:13:15 +0530

    update

--- PHASE2/Healthcare/src/demo/ExceptionDemo.java
+++ PHASE2/Healthcare/src/demo/ExceptionDemo.java
@@ -1,22 +1,42 @@
 package demo;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ExceptionDemo {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println(" Enter the plot size  ");
-		
-		int x = sc.nextInt();
-		
-		System.out.println(" In to how pieces you want divide this plot  ");
-		int b = sc.nextInt();
+//		Scanner sc = new Scanner(System.in);
+//
+//		System.out.println(" Enter the plot size  ");
+//
+//		int x = sc.nextInt();
+//
+//		System.out.println(" In to how pieces you want divide this plot  ");
+//		int b = sc.nextInt();
+//
+//		System.out.println(" So here size of each plot after division " + x / b);
+//
+//		System.out.println(" Thank you  ");
+
+		String readmeFilePath = "F:\\Users\\home\\git\\sl-oct23\\README.mdgrgrrtr";
+		FileReader fr = null;
 
 		
-		System.out.println(" So here size of each plot after division " + x / b);
-
+		try {
+			fr = new FileReader(readmeFilePath);
+			fr.read();
+		} catch (FileNotFoundException e) {
+			System.out.println("Inside FileNotFoundException catch block");
+			System.out.println(e.getMessage());
+		}
+		catch (IOException e) {
+			System.out.println("Inside IOException catch block");
+			System.out.println(e.getMessage());
+		}
+		
 		System.out.println(" Thank you  ");
 
 	}
