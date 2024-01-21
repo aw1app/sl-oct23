784664c84df1ddedd6c6e8fe570a538438bf7513
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-21 22:04:55 +0530

    update

---
+++ PHASE2/JDBCDemo/src/com/cms/JDBCQueryDemo.java
@@ -0,0 +1,22 @@
+package com.cms;
+
+public class JDBCQueryDemo {
+
+	public JDBCQueryDemo() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		//Steps to connect to our MySQL database named CMS
+		
+		//STEP 1: Loading the JDBC Driver
+		//STEP 2: Create a Connection
+		//STEP 3: Create a Statement 
+		//STEP 4: Execute the Statement (Query) and get the results (rows of data from the table)
+		//STEP 5: Display the results data by looping through the results(rows)
+		
+
+	}
+
+}
