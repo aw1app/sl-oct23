e24e96e767993e45f347c278a1be22e99238d866
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-21 22:09:11 +0530

    update

--- PHASE2/JDBCDemo/src/com/cms/JDBCQueryDemo.java
+++ PHASE2/JDBCDemo/src/com/cms/JDBCQueryDemo.java
@@ -1,21 +1,36 @@
 package com.cms;
 
+import java.sql.*;
+
 public class JDBCQueryDemo {
 
 	public JDBCQueryDemo() {
-		
+
 	}
 
 	public static void main(String[] args) {
-		
-		//Steps to connect to our MySQL database named CMS
-		
-		//STEP 1: Loading the JDBC Driver
-		//STEP 2: Create a Connection
-		//STEP 3: Create a Statement 
-		//STEP 4: Execute the Statement (Query) and get the results (rows of data from the table)
-		//STEP 5: Display the results data by looping through the results(rows)
-		
+
+		// Steps to connect to our MySQL database named CMS
+
+		// STEP 1: Loading the JDBC Driver
+		// STEP 2: Create a Connection
+		// STEP 3: Create a Statement
+		// STEP 4: Execute the Statement (Query) and get the results (rows of data from
+		// the table)
+		// STEP 5: Display the results data by looping through the results(rows)
+
+		Connection connection = null;
+
+		try {
+			// Step-1
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Step-2
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cms", "root", "rootroot");
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
