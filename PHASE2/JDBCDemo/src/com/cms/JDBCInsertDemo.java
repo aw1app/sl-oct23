ca7aedae6db8cadb93080adaabdbd8d4e82447be
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-21 22:53:20 +0530

    update

--- PHASE2/JDBCDemo/src/com/cms/JDBCInsertDemo.java
+++ PHASE2/JDBCDemo/src/com/cms/JDBCInsertDemo.java
@@ -16,7 +16,7 @@ public class JDBCInsertDemo {
 		// STEP 1: Loading the JDBC Driver
 		// STEP 2: Create a Connection
 		// STEP 3: Create a Statement
-		// STEP 4: Execute the Statement (Insert) 
+		// STEP 4: Execute the Statement (Insert)
 		// STEP 5: Display the successful insert message
 
 		Connection connection = null;
@@ -31,40 +31,48 @@ public class JDBCInsertDemo {
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		try {
-			//step-3
+			// step-3
 			Statement stmt = connection.createStatement();
-			
-			//step-4
+
+			// step-4
 			Scanner sc = new Scanner(System.in);
-			System.out.println("Id:");
-			int id = sc.nextInt();
-			System.out.println("first_name:");
-			String first_name = sc.next();
-			System.out.println("last_name:");
-			String last_name = sc.next();
-			System.out.println("date_of_birth");
-			String date_of_birth = sc.next();
-			System.out.println("enrollment_date:");
-			String enrollment_date = sc.next();
-			System.out.println("session_name:");
-			String session_name = sc.next();
+			while (true) {
+				System.out.println("Id:");
+				int id = sc.nextInt();
+				System.out.println("first_name:");
+				String first_name = sc.next();
+				System.out.println("last_name:");
+				String last_name = sc.next();
+				System.out.println("date_of_birth");
+				String date_of_birth = sc.next();
+				System.out.println("enrollment_date:");
+				String enrollment_date = sc.next();
+				System.out.println("session_name:");
+				String session_name = sc.next();
+
+				System.out.println("Insert another row?: Y / N :");
+				String answer = sc.next();
+				if (!answer.equalsIgnoreCase("Y"))
+					break;
+			}
 			sc.close();
-			
+
 			String sqlCommand = "INSERT INTO STUDENTS("
-					+ "student_id, first_name, last_name, date_of_birth, enrollment_date, session_name)"
-					+ " VALUES( " + id + ", '"+ first_name + "','" + last_name  + "','"+ date_of_birth + "','"+enrollment_date +"','" +session_name+"')";
-			
-			int noOfRowsInserted  = stmt.executeUpdate(sqlCommand);
-			
-			//step-5
-			System.out.println("Inserted "+noOfRowsInserted + " row(s) successfully!");			
-			
-			//CHALLENGE: Take the new row values like id, first_name . etc from command line
+					+ "student_id, first_name, last_name, date_of_birth, enrollment_date, session_name)" + " VALUES( "
+					+ id + ", '" + first_name + "','" + last_name + "','" + date_of_birth + "','" + enrollment_date
+					+ "','" + session_name + "')";
+
+			int noOfRowsInserted = stmt.executeUpdate(sqlCommand);
+
+			// step-5
+			System.out.println("Inserted " + noOfRowsInserted + " row(s) successfully!");
+
+			// CHALLENGE: Take the new row values like id, first_name . etc from command
+			// line
 			// and then insert the row.
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
