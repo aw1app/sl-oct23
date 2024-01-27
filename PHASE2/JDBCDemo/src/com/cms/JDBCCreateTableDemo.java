bcacc71e325e82fc861bb93fddfcb1910270dbe7
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-27 20:20:59 +0530

    update

--- PHASE2/JDBCDemo/src/com/cms/JDBCCreateTableDemo.java
+++ PHASE2/JDBCDemo/src/com/cms/JDBCCreateTableDemo.java
@@ -3,9 +3,9 @@ package com.cms;
 import java.sql.*;
 import java.util.Scanner;
 
-public class JDBCDeleteDemo {
+public class JDBCCreateTableDemo {
 
-	public JDBCDeleteDemo() {
+	public JDBCCreateTableDemo() {
 
 	}
 
@@ -16,8 +16,8 @@ public class JDBCDeleteDemo {
 		// STEP 1: Loading the JDBC Driver
 		// STEP 2: Create a Connection
 		// STEP 3: Create a Statement
-		// STEP 4: Execute the Statement (execute)
-		// STEP 5: Display the successful table creation message
+		// STEP 4: Execute the Statement (Insert)
+		// STEP 5: Display the successful insert message
 
 		Connection connection = null;
 
@@ -37,16 +37,27 @@ public class JDBCDeleteDemo {
 			Statement stmt = connection.createStatement();
 
 			// step-4
-			
-				String sqlCommand = "CREATE TABLE instructors(id int,first_name varchar(80)) ";
+			Scanner sc = new Scanner(System.in);
+			while (true) {
+				
+				System.out.println("ENTER row(student_id) delete? Enter -1 to quit :");				
+				int id = sc.nextInt();
+				
+				if(id==-1)break;				
+				
+				String sqlCommand = "DELETE FROM STUDENTS where student_id="+ id;
 
-				boolean result  = stmt.execute(sqlCommand);
+				int noOfRowsDeleted = stmt.executeUpdate(sqlCommand);
 
 				// step-5
-				if(result==false)
-				System.out.println("Created " + stmt.getUpdateCount() + " table(s) successfully!");			
-			
-			
+				System.out.println("Deleted " + noOfRowsDeleted + " row(s) successfully!");
+
+				System.out.println("Delete another row?: Y / N :");
+				String answer = sc.next();
+				if (!answer.equalsIgnoreCase("Y"))
+					break;
+			}
+			sc.close();
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
