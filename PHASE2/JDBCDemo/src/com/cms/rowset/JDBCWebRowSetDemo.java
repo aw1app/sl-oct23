da8fa5e210fd6cf05ff8cafc5956bea9d2b27092
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-28 21:30:38 +0530

    update

--- PHASE2/JDBCDemo/src/com/cms/rowset/JDBCWebRowSetDemo.java
+++ PHASE2/JDBCDemo/src/com/cms/rowset/JDBCWebRowSetDemo.java
@@ -1,5 +1,6 @@
 package com.cms.rowset;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
@@ -9,15 +10,49 @@ import java.sql.SQLException;
 import javax.sql.rowset.RowSetProvider;
 import javax.sql.rowset.WebRowSet;
 
-public class JDBCWebRowSetDemo {
-
+public class JDBCWebRowSetDemo  {
+	
 	public static void main(String[] args) {
+		//WebRowSet2XML();
+		
+		XML2WebRowSet();
+	}
+	
+	public static void XML2WebRowSet() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try  {
+			WebRowSet webRowSet = RowSetProvider.newFactory().createWebRowSet();
+			
+			webRowSet.readXml(new FileReader ("F:\\Users\\home\\git\\sl-oct23\\PHASE2\\JDBCDemo\\students.xml" ));
+			 
+			System.out.println(" Reading all rows from tghe saved xml file \n");
+			while (webRowSet.next()) {
+				int id = webRowSet.getInt("student_id");
+				String first_name = webRowSet.getString("first_name");
+				String lastName = webRowSet.getString("last_name");
+				String dateOfBirth = webRowSet.getString("date_of_birth");
+				String enrollmentDate = webRowSet.getString("enrollment_date");
+				String session_name = webRowSet.getString("session_name");
+
+				System.out.println(" ID: " + id + ", Name: " + first_name + " " + lastName + ", DOB: " + dateOfBirth
+						+ ", Enrollment Date: " + enrollmentDate + ", Session Name: " + session_name);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void WebRowSet2XML() {
 		// JDBC URL, username, and password of MySQL server
 		String jdbcUrl = "jdbc:mysql://localhost:3306/cms";
 		String username = "root";
 		String password = "rootroot";
-
-		
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -26,15 +61,14 @@ public class JDBCWebRowSetDemo {
 		}
 
 		try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
-            
 
 			WebRowSet webRowSet = RowSetProvider.newFactory().createWebRowSet();
-			
+
 			webRowSet.setUrl(jdbcUrl);
 			webRowSet.setUsername(username);
 			webRowSet.setPassword(password);
 
-			webRowSet.setCommand("select * from students");
+			webRowSet.setCommand("select * from students ");
 
 			webRowSet.execute();
 
@@ -52,18 +86,15 @@ public class JDBCWebRowSetDemo {
 			}
 
 			webRowSet.writeXml(new FileWriter("F:\\Users\\home\\git\\sl-oct23\\PHASE2\\JDBCDemo\\students.xml"));
-			
-			
-			webRowSet.close();
-			
 
-		} catch (SQLException  e) {
+			webRowSet.close();
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Inside SQLException block ");
 			System.out.println(e.getMessage());
-			System.out.println(e.getErrorCode());			
-			
-		}catch ( IOException e) {
+			System.out.println(e.getErrorCode());
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
