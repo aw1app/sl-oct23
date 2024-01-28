2d5af089dcc6eeb4a264d5ccc89a9d5c108c92a1
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-28 20:14:30 +0530

    update

--- PHASE2/JDBCDemo/src/com/cms/rowset/JDBCRowSetDemo.java
+++ PHASE2/JDBCDemo/src/com/cms/rowset/JDBCRowSetDemo.java
@@ -17,63 +17,106 @@ public class JDBCRowSetDemo {
 		Connection connection = null;
 
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");			
-		} catch (ClassNotFoundException  e) {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 
 		try {
-			
+
 			JdbcRowSet jdbcRowSet = RowSetProvider.newFactory().createJdbcRowSet();
 			jdbcRowSet.setUrl(jdbcUrl);
 			jdbcRowSet.setUsername(username);
 			jdbcRowSet.setPassword(password);
-			
+
 			jdbcRowSet.setCommand("select * from students");
-			
+
 			jdbcRowSet.execute();
-			
-			System.out.println(" Fetch all rows \n" );
-			while(jdbcRowSet.next()) {
-				int id  = jdbcRowSet.getInt("student_id");
-				String first_name  = jdbcRowSet.getString("first_name");
+
+			System.out.println(" Fetch all rows \n");
+			while (jdbcRowSet.next()) {
+				int id = jdbcRowSet.getInt("student_id");
+				String first_name = jdbcRowSet.getString("first_name");
 				String lastName = jdbcRowSet.getString("last_name");
-	            String dateOfBirth = jdbcRowSet.getString("date_of_birth");
-	            String enrollmentDate = jdbcRowSet.getString("enrollment_date");
-	            String session_name = jdbcRowSet.getString("session_name");
-	            
-	            System.out.println(" ID: " + id +
-	                    ", Name: " + first_name + " " + lastName +
-	                    ", DOB: " + dateOfBirth +
-	                    ", Enrollment Date: " + enrollmentDate +
-	                    ", Session Name: " + session_name
-	                    );
+				String dateOfBirth = jdbcRowSet.getString("date_of_birth");
+				String enrollmentDate = jdbcRowSet.getString("enrollment_date");
+				String session_name = jdbcRowSet.getString("session_name");
+
+				System.out.println(" ID: " + id + ", Name: " + first_name + " " + lastName + ", DOB: " + dateOfBirth
+						+ ", Enrollment Date: " + enrollmentDate + ", Session Name: " + session_name);
+			}
+
+			System.out.println("\n\n ");
+
+			// Go to the 3rd row
+			System.out.println(" Fetch the 3rd row \n ");
+			jdbcRowSet.absolute(3);
+			int id = jdbcRowSet.getInt("student_id");
+			String first_name = jdbcRowSet.getString("first_name");
+			String lastName = jdbcRowSet.getString("last_name");
+			String dateOfBirth = jdbcRowSet.getString("date_of_birth");
+			String enrollmentDate = jdbcRowSet.getString("enrollment_date");
+			String session_name = jdbcRowSet.getString("session_name");
+
+			System.out.println(" ID: " + id + ", Name: " + first_name + " " + lastName + ", DOB: " + dateOfBirth
+					+ ", Enrollment Date: " + enrollmentDate + ", Session Name: " + session_name);
+
+			// Challenge: After going to the 3rd row How will you display rows(all rows) in
+			// the reverse order
+
+			System.out.println("\n\n ");
+			System.out.println(" Fetch all rows in reverse from the current row (3) \n ");
+			
+			while (jdbcRowSet.previous()) {
+				id = jdbcRowSet.getInt("student_id");
+				first_name = jdbcRowSet.getString("first_name");
+				String last_name = jdbcRowSet.getString("last_name");
+				String dob = jdbcRowSet.getString("date_of_birth");
+				String edate = jdbcRowSet.getString("enrollment_date");
+				String sname = jdbcRowSet.getString("session_name");
+
+				System.out.println("ID: " + id + ", First Name: " + first_name + ", Last Name: " + last_name
+						+ ", Date Of Birth: " + dob + ", Date Of Enrollment: " + edate + ", Session Name: " + sname);
 			}
 			
-			System.out.println("\n\n ");
 			
-			// Go to the 3rd row
-			System.out.println(" Fetch the 3rd row \n " );
-			jdbcRowSet.absolute(3);
-			int id  = jdbcRowSet.getInt("student_id");
-			String first_name  = jdbcRowSet.getString("first_name");
-			String lastName = jdbcRowSet.getString("last_name");
-            String dateOfBirth = jdbcRowSet.getString("date_of_birth");
-            String enrollmentDate = jdbcRowSet.getString("enrollment_date");
-            String session_name = jdbcRowSet.getString("session_name");
-            
-            System.out.println(" ID: " + id +
-                    ", Name: " + first_name + " " + lastName +
-                    ", DOB: " + dateOfBirth +
-                    ", Enrollment Date: " + enrollmentDate +
-                    ", Session Name: " + session_name
-                    );
-            
-            // Challenge: After going to the 3rd row How will display rows in the reverse order
-            
+			System.out.println("\n\n ");
+			System.out.println(" Fetch all rows in reverse from the end\n ");
+			
+			jdbcRowSet.afterLast();
+			
+			while (jdbcRowSet.previous()) {
+				id = jdbcRowSet.getInt("student_id");
+				first_name = jdbcRowSet.getString("first_name");
+				String last_name = jdbcRowSet.getString("last_name");
+				String dob = jdbcRowSet.getString("date_of_birth");
+				String edate = jdbcRowSet.getString("enrollment_date");
+				String sname = jdbcRowSet.getString("session_name");
+
+				System.out.println("ID: " + id + ", First Name: " + first_name + ", Last Name: " + last_name
+						+ ", Date Of Birth: " + dob + ", Date Of Enrollment: " + edate + ", Session Name: " + sname);
+			};
+			
+			// 
+			System.out.println("\n\n ");
+			System.out.println(" Inserting a new record \n ");
+			
+			jdbcRowSet.clearParameters();
+			jdbcRowSet.setInt("student_id", 20);
+			jdbcRowSet.setString("first_name", "A20");
+			jdbcRowSet.setString("last_name", "B20");
+			jdbcRowSet.setString("date_of_birth", "1999-01-01");
+			jdbcRowSet.setString("enrollment_date", "2024-01-01");
+			jdbcRowSet.setString("session_name", "ZUMBA");
+			
+			jdbcRowSet.insertRow();
+			
 
 		} catch (SQLException e) {
-
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+			System.out.println(e.getErrorCode());			
+			
 		}
 
 	}
