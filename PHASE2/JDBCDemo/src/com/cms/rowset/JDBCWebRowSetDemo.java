3866f1bd259c89a83cfb2d3b8864e096ab82ec61
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-28 21:02:26 +0530

    update

---
+++ PHASE2/JDBCDemo/src/com/cms/rowset/JDBCWebRowSetDemo.java
@@ -0,0 +1,72 @@
+package com.cms.rowset;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.sql.rowset.RowSetProvider;
+import javax.sql.rowset.WebRowSet;
+
+public class JDBCWebRowSetDemo {
+
+	public static void main(String[] args) {
+		// JDBC URL, username, and password of MySQL server
+		String jdbcUrl = "jdbc:mysql://localhost:3306/cms";
+		String username = "root";
+		String password = "rootroot";
+
+		
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            
+
+			WebRowSet webRowSet = RowSetProvider.newFactory().createWebRowSet();
+			
+			webRowSet.setUrl(jdbcUrl);
+			webRowSet.setUsername(username);
+			webRowSet.setPassword(password);
+
+			webRowSet.setCommand("select * from students");
+
+			webRowSet.execute();
+
+			System.out.println(" Fetch all rows \n");
+			while (webRowSet.next()) {
+				int id = webRowSet.getInt("student_id");
+				String first_name = webRowSet.getString("first_name");
+				String lastName = webRowSet.getString("last_name");
+				String dateOfBirth = webRowSet.getString("date_of_birth");
+				String enrollmentDate = webRowSet.getString("enrollment_date");
+				String session_name = webRowSet.getString("session_name");
+
+				System.out.println(" ID: " + id + ", Name: " + first_name + " " + lastName + ", DOB: " + dateOfBirth
+						+ ", Enrollment Date: " + enrollmentDate + ", Session Name: " + session_name);
+			}
+
+			webRowSet.writeXml(new FileWriter("F:\\Users\\home\\git\\sl-oct23\\PHASE2\\JDBCDemo\\students.xml"));
+			
+			
+			webRowSet.close();
+			
+
+		} catch (SQLException  e) {
+			e.printStackTrace();
+			System.out.println("Inside SQLException block ");
+			System.out.println(e.getMessage());
+			System.out.println(e.getErrorCode());			
+			
+		}catch ( IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
