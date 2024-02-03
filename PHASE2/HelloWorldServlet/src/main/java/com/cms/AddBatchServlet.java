acf02cc1ef7b41ab621ef3b5ebbda6e6964ac2b6
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-03 22:20:23 +0530

    update

--- PHASE2/HelloWorldServlet/src/main/java/com/cms/AddBatchServlet.java
+++ PHASE2/HelloWorldServlet/src/main/java/com/cms/AddBatchServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,54 +15,63 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AddBatchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
+	public void init(ServletConfig config) {
+
+	}
+	
+	@Override
+	public void destroy() {
+
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		
+
 		String batchName = request.getParameter("batch-name");
 		String batchInstructor = request.getParameter("batch-instructor");
 		String batchStart = request.getParameter("batch-start");
 		String batchEnd = request.getParameter("batch-start");
 		String batchType = request.getParameter("batch-type");
-		
+
 		// To process a checkbox, which is multivalued, use request.getParameterValues.
 		// Same for multi- selection list
-		String[] batchPrerquesties = request.getParameterValues("batch-prerequistes"); 
-		
-		out.println("<p> Thank you for adding a new batch!</p>");
-		
-		out.printf(" You Submitted BatchName = %s %s %s %s %s %s <br>",
-				batchName,batchInstructor,batchStart,batchEnd,batchType, Arrays.toString(batchPrerquesties ));
+		String[] batchPrerquesties = request.getParameterValues("batch-prerequistes");
 
-		out.close();		
+		out.println("<p> Thank you for adding a new batch!</p>");
+
+		out.printf(" You Submitted BatchName = %s %s %s %s %s %s <br>", batchName, batchInstructor, batchStart,
+				batchEnd, batchType, Arrays.toString(batchPrerquesties));
+
+		out.close();
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
+
 	/*
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		
-		String batchName = request.getParameter("batch-name");
-		String batchInstructor = request.getParameter("batch-instructor");
-		String batchStart = request.getParameter("batch-start");
-		String batchEnd = request.getParameter("batch-start");
-		String batchType = request.getParameter("batch-type");
-		
-		out.println("<p> Thank you for adding a new batch!</p>");
-		
-		out.printf(" You Submitted BatchName = %s %s %s %s %s <br>",batchName,batchInstructor,batchStart,batchEnd,batchType  );
-
-		out.close();
-	}
-	*/
+	 * protected void doPost(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException {
+	 * 
+	 * response.setContentType("text/html"); PrintWriter out = response.getWriter();
+	 * 
+	 * String batchName = request.getParameter("batch-name"); String batchInstructor
+	 * = request.getParameter("batch-instructor"); String batchStart =
+	 * request.getParameter("batch-start"); String batchEnd =
+	 * request.getParameter("batch-start"); String batchType =
+	 * request.getParameter("batch-type");
+	 * 
+	 * out.println("<p> Thank you for adding a new batch!</p>");
+	 * 
+	 * out.printf(" You Submitted BatchName = %s %s %s %s %s <br>",batchName,
+	 * batchInstructor,batchStart,batchEnd,batchType );
+	 * 
+	 * out.close(); }
+	 */
 
 }
---
+++ PHASE2/HelloWorldServlet/src/main/java/com/cms/ServletLifeCycleDemo.java
@@ -0,0 +1,28 @@
+package com.cms;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = { "/lcDemo" })
+public class ServletLifeCycleDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init(ServletConfig config) {
+		System.out.println("INSIDE init ");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("INSIDE destroy ");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("INSIDE do Get ");
+	}
+
+}
