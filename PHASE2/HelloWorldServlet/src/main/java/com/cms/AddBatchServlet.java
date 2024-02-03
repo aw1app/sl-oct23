af577318c0040e2049adb2ddf5d0ea81cfcb32d8
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-03 20:56:56 +0530

    update

---
+++ PHASE2/HelloWorldServlet/src/main/java/com/cms/AddBatchServlet.java
@@ -0,0 +1,61 @@
+package com.cms;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/addBatch" })
+public class AddBatchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String batchName = request.getParameter("batch-name");
+		String batchInstructor = request.getParameter("batch-instructor");
+		String batchStart = request.getParameter("batch-start");
+		String batchEnd = request.getParameter("batch-start");
+		String batchType = request.getParameter("batch-type");
+		
+		out.println("<p> Thank you for adding a new batch!</p>");
+		
+		out.printf(" You Submitted BatchName = %s %s %s %s %s <br>",batchName,batchInstructor,batchStart,batchEnd,batchType  );
+
+		out.close();		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	/*
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String batchName = request.getParameter("batch-name");
+		String batchInstructor = request.getParameter("batch-instructor");
+		String batchStart = request.getParameter("batch-start");
+		String batchEnd = request.getParameter("batch-start");
+		String batchType = request.getParameter("batch-type");
+		
+		out.println("<p> Thank you for adding a new batch!</p>");
+		
+		out.printf(" You Submitted BatchName = %s %s %s %s %s <br>",batchName,batchInstructor,batchStart,batchEnd,batchType  );
+
+		out.close();
+	}
+	*/
+
+}
