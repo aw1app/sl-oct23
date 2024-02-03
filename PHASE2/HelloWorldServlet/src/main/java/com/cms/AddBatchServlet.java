54592ea159827ab8cc04ff8874abe02ddeea8227
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-03 21:50:40 +0530

    update

--- PHASE2/HelloWorldServlet/src/main/java/com/cms/AddBatchServlet.java
+++ PHASE2/HelloWorldServlet/src/main/java/com/cms/AddBatchServlet.java
@@ -2,6 +2,7 @@ package com.cms;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -25,9 +26,14 @@ public class AddBatchServlet extends HttpServlet {
 		String batchEnd = request.getParameter("batch-start");
 		String batchType = request.getParameter("batch-type");
 		
+		// To process a checkbox, which is multivalued, use request.getParameterValues.
+		// Same for multi- selection list
+		String[] batchPrerquesties = request.getParameterValues("batch-prerequistes"); 
+		
 		out.println("<p> Thank you for adding a new batch!</p>");
 		
-		out.printf(" You Submitted BatchName = %s %s %s %s %s <br>",batchName,batchInstructor,batchStart,batchEnd,batchType  );
+		out.printf(" You Submitted BatchName = %s %s %s %s %s %s <br>",
+				batchName,batchInstructor,batchStart,batchEnd,batchType, Arrays.toString(batchPrerquesties ));
 
 		out.close();		
 	}
