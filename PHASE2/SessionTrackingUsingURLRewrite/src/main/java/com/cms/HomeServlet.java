5ee229d5cc2654a46963eb5f999e58a81eae7da9
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-04 22:04:57 +0530

    update

---
+++ PHASE2/SessionTrackingUsingURLRewrite/src/main/java/com/cms/HomeServlet.java
@@ -0,0 +1,26 @@
+package com.cms;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+
+public class HomeServlet extends HttpServlet {
+	Random random = new Random();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();		
+		int sessId = random.nextInt();
+		
+		out.print("Content from HomeServlet ");
+		
+		response.sendRedirect("DashboardServlet?sessId="+sessId);
+		
+		out.close();
+	}
+
+}
