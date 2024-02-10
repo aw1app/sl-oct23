66fcbdfc47e44f42c5d6b3669ca112d30c754e1d
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-10 20:13:59 +0530

    update

---
+++ PHASE2/SessionTrackingUsingHTTPSessionDemo/src/main/java/com/ecommerce/LogoutServlet.java
@@ -0,0 +1,30 @@
+package com.ecommerce;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		HttpSession session  = request.getSession(false);
+		
+		if(session!=null)
+			session.invalidate();
+		
+		out.print("<center>Logout successful.</center>");
+		//out.print("<br> <a href=\"login.html\">Re Login</a>");
+		out.print("<br> <a href=\"index.html\">Go to Index Page</a>");
+	}
+    
+
+}
