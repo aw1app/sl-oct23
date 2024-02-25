07399911fb7cc4e2a3049fab79386e2646ad9ff1
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-25 20:50:58 +0530

    update

--- PHASE2/my-maven-mongodb-app/src/main/java/com/example/App.java
+++ PHASE2/my-maven-mongodb-app/src/main/java/com/example/App.java
@@ -1,13 +1,58 @@
 package com.example;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-        
-        
-        
-    }
+import org.bson.Document;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Mongo!");
+		// Connect to MongoDB
+		try {
+
+			MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+			MongoDatabase database = mongoClient.getDatabase("mydatabase");
+			MongoCollection<Document> usersCollection = database.getCollection("users");
+
+			//listAllDocumentsDemo(usersCollection);
+			
+			insertOneDocumentDemo(usersCollection, "USER101","USER101@gmail.com",26);
+			
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public static void insertOneDocumentDemo(MongoCollection<Document> collection, String username, String email,
+			int age) {
+		
+		Document user = new Document();
+		
+		user
+		.append("email", email)
+		.append("username", username)
+		.append("age", age);
+		
+		collection.insertOne(user);
+	}
+
+	public static void listAllDocumentsDemo(MongoCollection<Document> collection) {
+		FindIterable<Document> users = collection.find();
+
+		for (Document user : users) {
+			System.out.println("** User **");
+			System.out.println("Age: " + user.getInteger("age"));
+			System.out.println("Username:  " + user.getString("username"));
+			System.out.println("Email :  " + user.getString("email"));
+			System.out.println("");
+		}
+
+	}
+
 }
