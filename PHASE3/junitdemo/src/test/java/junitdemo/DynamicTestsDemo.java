59103c6cbdd3a22585e802b1016b6788284e2b1e
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-10 22:27:28 +0530

    update

---
+++ PHASE3/junitdemo/src/test/java/junitdemo/DynamicTestsDemo.java
@@ -0,0 +1,25 @@
+package junitdemo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+class DynamicTestsDemo {
+
+	@TestFactory
+	Collection<DynamicTest> dynamicTests1() {
+		
+		DynamicTest dynTest1 = dynamicTest("Dynamic test 1", () -> { assertTrue(7 == new Calculator().add(2, 5)) ; } );
+		
+		DynamicTest dynTest2 = dynamicTest("Dynamic test 2 for division", () -> { assertTrue(2 == new Calculator().divide(5, 2)) ; } );
+		
+		
+		return Arrays.asList(dynTest1, dynTest2);		
+	}
+
+}
