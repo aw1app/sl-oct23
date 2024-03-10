99e484006fcc81d96bec4c022f4c94c06a42f8b4
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-10 21:26:34 +0530

    update

--- PHASE3/junitdemo/src/test/java/junitdemo/CalculatorTest.java
+++ PHASE3/junitdemo/src/test/java/junitdemo/CalculatorTest.java
@@ -1,19 +1,37 @@
 package junitdemo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+	Calculator calulator1;
+	
+	
+	@BeforeEach
+	void myBeforeEach() {
+		System.out.println(" INSIDE myBeforeEach ");
+		calulator1 = new Calculator();
+	}
+	
+	@AfterEach
+	void myAfterEach() {
+		System.out.println(" INSIDE myAfterEach ");
+		calulator1 = null;
+	}
+	
 
 	@Test
 	void test1Add() {
+		System.out.println(" INSIDE test1Add ");
 		
 		// Test the calculator class for add functionality.
 		// Pass parameters 4 and 5 to the add method.
 		// We should expect 9 to be returned.
 		
-		Calculator calulator1 = new Calculator();		
+		//Calculator calulator1 = new Calculator();		
 		
 		int actualResult = calulator1.add(4,5);
 		
@@ -25,12 +43,13 @@ class CalculatorTest {
 	
 	@Test
 	void test2Add() {
+		System.out.println(" INSIDE test2Add ");
 		
 		// Test the calculator class for add functionality.
 		// Pass parameters -1 and 5 to the add method.
 		// We should expect 4 to be returned.
 		
-		Calculator calulator1 = new Calculator();		
+		//Calculator calulator1 = new Calculator();		
 		
 		int actualResult = calulator1.add(-1,5);
 		
@@ -41,5 +60,19 @@ class CalculatorTest {
 	}
 	
 	//Challenge: Write two more test cases for add method
+	@Test
+	void test3Add() {
+		System.out.println(" INSIDE test3Add ");
+		
+		// Test the calculator class for add functionality.
+		// Pass parameters positive 5 and negative -10 to the add method.
+		// We should expect -5 to be returned.
+		
+		//Calculator calulator1 = new Calculator();
+		int actualResult = calulator1.add(5,-10);
+		int expectedResult = -5;
+		assertEquals(expectedResult, actualResult);
+	}
+
 
 }
