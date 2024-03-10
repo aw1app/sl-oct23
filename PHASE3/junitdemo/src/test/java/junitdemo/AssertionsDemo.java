b64d6f50382992c08b1b60e65feffaab4fb874cd
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-10 21:56:23 +0530

    update

---
+++ PHASE3/junitdemo/src/test/java/junitdemo/AssertionsDemo.java
@@ -0,0 +1,46 @@
+package junitdemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AssertionsDemo {
+
+	@Test
+	void test() {
+
+		String str = null;
+		String str2 = "some value";
+
+		String[] a1 = { "A", "B" };
+		String[] a2 = { "A", "B" };
+
+		int a = 4;
+		int b = 0;
+
+		assertTrue(a > b);
+
+		assertFalse(5 < 1);
+
+		assertNull(str);
+		assertNotNull(str2);
+
+		assertSame(str, str);
+		assertNotSame(str, str2);
+
+		assertEquals(5, 5);
+		assertNotEquals(5, 6);
+
+		assertArrayEquals(a1, a2);
+
+		assertThrows(RuntimeException.class, () -> {
+			throw new RuntimeException();
+		});
+		
+		assertThrows(ArithmeticException.class, () -> {
+			new Calculator().divide(10, 0);
+		});
+
+	}
+
+}
--- PHASE3/junitdemo/src/test/java/junitdemo/CalculatorTest.java
+++ PHASE3/junitdemo/src/test/java/junitdemo/CalculatorTest.java
@@ -1,12 +1,8 @@
 package junitdemo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class CalculatorTest {
 	Calculator calulator1;
