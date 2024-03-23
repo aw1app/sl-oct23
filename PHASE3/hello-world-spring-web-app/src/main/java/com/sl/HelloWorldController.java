aaefeccbe651e00da5d9430f45f7f7b3c6235b28
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-23 19:45:42 +0530

    update

--- PHASE3/hello-world-spring-web-app/src/main/java/com/sl/HelloWorldController.java
+++ PHASE3/hello-world-spring-web-app/src/main/java/com/sl/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.sl;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,8 +14,30 @@ public class HelloWorldController {
 	@ResponseBody
 	public String hello() {
 		
-		return "Hi there";
+		return "hello :Hi there";
 	}
 	
+	
+	@GetMapping("/hello2")
+	@ResponseBody
+	public String hello2() {
+		
+		return "hello2 : Hi there";
+	}
+	
+	@RequestMapping(name="/hello3", method=RequestMethod.GET)
+	@ResponseBody
+	public String hello3() {
+		
+		return "hello3 : Hi there";
+	}
+	
+	
+	@RequestMapping(name="/hello4", method=RequestMethod.POST)
+	@ResponseBody
+	public String hello4() {
+		
+		return "hello4: Hi there";
+	}
 
 }
