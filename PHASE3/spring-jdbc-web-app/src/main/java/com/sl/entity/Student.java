82af4d69570e79c7330f67b69210296a0349fcbc
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-23 21:42:40 +0530

    update

---
+++ PHASE3/spring-jdbc-web-app/src/main/java/com/sl/entity/Student.java
@@ -0,0 +1,62 @@
+package com.sl.entity;
+
+import java.util.Date;
+
+public class Student {
+	
+	int student_id;
+	String first_name;
+	String last_name;
+	Date date_of_birth;
+	Date enrollment_date;
+	String session_name;
+
+	public int getStudent_id() {
+		return student_id;
+	}
+
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public Date getDate_of_birth() {
+		return date_of_birth;
+	}
+
+	public void setDate_of_birth(Date date_of_birth) {
+		this.date_of_birth = date_of_birth;
+	}
+
+	public Date getEnrollment_date() {
+		return enrollment_date;
+	}
+
+	public void setEnrollment_date(Date enrollment_date) {
+		this.enrollment_date = enrollment_date;
+	}
+
+	public String getSession_name() {
+		return session_name;
+	}
+
+	public void setSession_name(String session_name) {
+		this.session_name = session_name;
+	}
+
+}
