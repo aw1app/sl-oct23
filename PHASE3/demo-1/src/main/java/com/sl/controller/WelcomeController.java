01a096c31d087b395ba2982b5bf17744a84a777d
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-24 20:26:10 +0530

    update

--- PHASE3/demo-1/src/main/java/com/sl/controller/WelcomeController.java
+++ PHASE3/demo-1/src/main/java/com/sl/controller/WelcomeController.java
@@ -1,13 +1,16 @@
 package com.sl.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 	
 	@GetMapping("/welcome")
-	public String welcome() {
+	public String welcome(Model model, String name) {
+		
+		model.addAttribute("name", name);
 		
 		return "welcome" ; // display WEB-INF/views/welcome.jsp
 	}
