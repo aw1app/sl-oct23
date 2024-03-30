759bf391a321cf72a1766513b6d8851098cb613a
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-30 22:52:56 +0530

    update

--- PHASE3/spring-boot-rest-api-server/src/main/java/com/sl/app/StudentRestController.java
+++ PHASE3/spring-boot-rest-api-server/src/main/java/com/sl/app/StudentRestController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +38,14 @@ public class StudentRestController {
 		}
 
 		return null;
-
+	}
+	
+	// Add a student
+	@PostMapping("/add")
+	public Student addStudent(@RequestBody Student student) {
+		student = studentRepositry.save(student);
+		
+		return student;
 	}
 
 }
