fdffdb9af16c256ddf667b5ff7dc4d7b3e9df814
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-30 19:41:58 +0530

    update

--- PHASE3/spring-boot-jpa-demo/src/main/java/com/sl/app/StudentController.java
+++ PHASE3/spring-boot-jpa-demo/src/main/java/com/sl/app/StudentController.java
@@ -12,54 +12,67 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
-	
+
 	@Autowired
 	StudentRepositry studentRepositry;
-	
-		
-	
+
 	@GetMapping("/list")
 	public String listAllStudents(Model model) {
-		
+
 		List<Student> students = studentRepositry.findAll();
-		
+
 		model.addAttribute("students", students);
-		
+
 		return "list-students";
-	} 
-	
+	}
+
 	@GetMapping("/details")
 	public String studentDetails(Model model, int id) {
-		
+
 		Optional<Student> studentFromRepo = studentRepositry.findById(id);
-		
-		if(studentFromRepo.isPresent()) {
+
+		if (studentFromRepo.isPresent()) {
 			Student student = studentFromRepo.get();
 			model.addAttribute("student", student);
-		}		
-		
+		}
+
 		return "student-detail";
 	}
-	
+
 	@GetMapping("/add-student")
 	public String addStudentShowForm(Model model) {
-		
+
 		Student student = new Student();
 		model.addAttribute("student", student);
-		
+
 		return "add-student";
 	}
-	
+
 	@PostMapping("/add-student")
-	public String addStudent(Model model, @ModelAttribute("student") Student student) {	
-		
-		
+	public String addStudent(Model model, @ModelAttribute("student") Student student) {
+
 		student = studentRepositry.save(student);
-		
+
 		model.addAttribute("studentId", student.getStudentId());
-		
+
 		return "add-student-success";
 	}
-	
+
+	// EDIT FUNCTIONALITY
+	@GetMapping("/edit-student")
+	public String editStudentShowForm(Model model, int id) {
+
+		Optional<Student> studentFromRepo = studentRepositry.findById(id);
+
+		if (studentFromRepo.isPresent()) {
+			Student student = studentFromRepo.get();
+			model.addAttribute("student", student);
+			return "edit-student";
+		} else {
+			model.addAttribute("id", id);
+			return "no-such-student";
+		}
+
+	}
 
 }
