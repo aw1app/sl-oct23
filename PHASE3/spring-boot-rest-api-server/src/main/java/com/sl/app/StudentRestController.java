2ef2392f21efeb21d75fa3726e845b26d085fede
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-30 22:20:10 +0530

    update

---
+++ PHASE3/spring-boot-rest-api-server/src/main/java/com/sl/app/StudentRestController.java
@@ -0,0 +1,26 @@
+package com.sl.app;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentRestController {
+	
+	@Autowired
+	StudentRepositry studentRepositry;
+	
+	@GetMapping("/all")
+	public List<Student> listAllStudents() {
+
+		List<Student> students = studentRepositry.findAll();		
+
+		return students;
+	}
+	
+}
