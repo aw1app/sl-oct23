09844392f3c81388bb1eaee02e966e87ecae817f
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-30 22:30:21 +0530

    update

--- PHASE3/spring-boot-rest-api-server/src/main/java/com/sl/app/StudentRestController.java
+++ PHASE3/spring-boot-rest-api-server/src/main/java/com/sl/app/StudentRestController.java
@@ -1,29 +1,42 @@
 package com.sl.app;
 
-
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/students")
 public class StudentRestController {
-	
+
 	@Autowired
 	StudentRepositry studentRepositry;
-	
+
 	@GetMapping("/all")
 	public List<Student> listAllStudents() {
 
-		List<Student> students = studentRepositry.findAll();		
+		List<Student> students = studentRepositry.findAll();
 
 		return students;
 	}
-	
+
 	// Challenge : Fetch details of a single student
 	@GetMapping("/student/{id}")
-	
+	public Student getStudent(@PathVariable int id) {
+
+		Optional<Student> studentFromRepo = studentRepositry.findById(id);
+
+		if (studentFromRepo.isPresent()) {
+			Student student = studentFromRepo.get();
+			return student;
+		}
+
+		return null;
+
+	}
+
 }
