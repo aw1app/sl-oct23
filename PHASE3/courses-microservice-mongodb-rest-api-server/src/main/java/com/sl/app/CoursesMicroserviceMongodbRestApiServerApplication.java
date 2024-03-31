a0243b33990ea62192bad5562cbd56b3524b9986
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-31 21:37:14 +0530

    update

---
+++ PHASE3/courses-microservice-mongodb-rest-api-server/src/main/java/com/sl/app/CoursesMicroserviceMongodbRestApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.sl.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesMicroserviceMongodbRestApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesMicroserviceMongodbRestApiServerApplication.class, args);
+	}
+
+}
