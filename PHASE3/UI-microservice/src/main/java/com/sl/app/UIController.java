af3dfe7e53e1f0b331be0affa5d3edfbbbfe686c
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-31 23:11:07 +0530

    update

--- PHASE3/UI-microservice/src/main/java/com/sl/app/UIController.java
+++ PHASE3/UI-microservice/src/main/java/com/sl/app/UIController.java
@@ -47,21 +47,3 @@ public class UIController {
 
 }
 
-
-
-class Course{
-	String id;
-	String name;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}	
-}
