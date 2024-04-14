f55d305df885959c4364a598318fc90c370b96c3
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-04-14 21:25:26 +0530

    update

--- PHASE3/demo-soap/src/main/java/com/sl/CalculatorServiceImpl.java
+++ PHASE3/demo-soap/src/main/java/com/sl/CalculatorServiceImpl.java
@@ -1,13 +1,18 @@
 package com.sl;
 
 public class CalculatorServiceImpl implements CalculatorService {
-	
+
 	Calculator calc = new Calculator();
 
 	@Override
 	public int add(int x, int y) {
-		
+
 		return calc.add(x, y);
 	}
-	
+
+	public int multiply(int x, int y) {
+
+		return x * y;
+	}
+
 }
